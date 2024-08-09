68d21991f6dd12e5e3cb44aec257e09f3334faec
mine702 <107476541+mine702@users.noreply.github.com>
2024-08-09 15:47:06 +0900

    Delete hello_world/target directory

--- hello_world/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-C:\Users\sjh\Desktop\learn_rust\hello_world\target\debug\deps\hello_world.exe: src/main.rs
-
-C:\Users\sjh\Desktop\learn_rust\hello_world\target\debug\deps\hello_world.d: src/main.rs
-
-src/main.rs:
