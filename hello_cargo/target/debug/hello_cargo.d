60847d45202a635c231cceaf0bd011a8797332d5
mine <mine702@naver.com>
2024-08-09 13:06:52 +0900

    hello_cargo

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\sjh\Desktop\learn_rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\sjh\Desktop\learn_rust\hello_cargo\src\main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\sjh\Desktop\learn_rust\hello_cargo\target\release\hello_cargo.exe: C:\Users\sjh\Desktop\learn_rust\hello_cargo\src\main.rs
