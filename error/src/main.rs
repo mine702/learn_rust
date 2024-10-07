64177229ff0150421c8aaf318875660ca72581c7
mine <mine702@naver.com>
2024-10-07 17:55:00 +0900

    Result와 함께하는 복구 가능한 에러

--- error/src/main.rs
+++ error/src/main.rs
@@ -1,9 +1,80 @@
+use std::fs::File;
+use std::io;
+use std::io::ErrorKind;
+use std::io::Read;
+
 fn main() {
-    error_1();
+    // error_1();
+    // error_2();
+    // error_3();
+    // error_4();
+    read_username_from_file();
 }
 
 fn error_1() {
     // panic!("crash and burn");
     let v = vec![1, 2, 3];
     v[99];
+}
+
+fn error_2() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("파일을 열 수 없습니다: {:?}", error)
+        }
+    };
+}
+
+fn error_3() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("파일을 생성할 수 없습니다: {:?}", e),
+            }
+        }
+        Err(error) => {
+            panic!("파일을 열 수 없습니다: {:?}", error)
+        }
+    };
+}
+
+fn error_4() {
+    let f = File::open("hello.txt").unwrap();
+    let f = File::open("hello.txt").expect("파일을 열 수 없습니다.");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file3() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
 }
