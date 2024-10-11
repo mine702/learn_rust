5f2b6cf36932c0fcec2ce237bd08ba82850d4f14
mine <mine702@naver.com>
2024-10-11 14:49:40 +0900

    panic!이냐, panic!이 아니냐, 그것이 문제로다

--- error/src/main.rs
+++ error/src/main.rs
@@ -1,7 +1,10 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io;
 use std::io::ErrorKind;
 use std::io::Read;
+use std::net::IpAddr;
 
 fn main() {
     // error_1();
@@ -48,6 +51,66 @@ fn error_3() {
 fn error_4() {
     let f = File::open("hello.txt").unwrap();
     let f = File::open("hello.txt").expect("파일을 열 수 없습니다.");
+}
+
+fn error_5() {
+    let home = "127.0.0.1".parse::<IpAddr>().unwrap();
+}
+
+fn error_6() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng()
+        .gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if guess < 1 || guess > 100 {
+            println!("숫자를 1과 100 사이로 입력해주세요.");
+            continue;
+        }
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
+
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
